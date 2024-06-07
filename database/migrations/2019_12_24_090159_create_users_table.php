6e9621ea9a72e4a5bba042234e18ee956d6b8bee
mijinko0 <tokesota@gmail.com>
2024-06-07 15:38:03 +0900

    命名規則保険作成

--- database/migrations/2019_12_24_090159_create_users_table.php
+++ database/migrations/2019_12_24_090159_create_users_table.php
@@ -11,7 +11,8 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+
+     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
@@ -24,6 +25,20 @@ class CreateUsersTable extends Migration
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
         });
     }
+     /*---命名規則的にこっちの方が正しい？？【2024/06/06】
+    public function up()
+    {
+        Schema::create('users_table', function (Blueprint $table) {
+            $table->integer('user_id')->autoIncrement();
+            $table->string('user_name',255);
+            $table->string('user_mail',255);
+            $table->string('user_password',255);
+            $table->string('user_bio',400)->nullable();
+            $table->string('user_images',255)->default('icon1.png');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
+        });
+    } */
 
     /**
      * Reverse the migrations.
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -38,5 +38,24 @@ class UsersTableSeeder extends Seeder
             'mail' => 'Atlas05@mail.com',
             'password' => bcrypt(5000)]
         ]);
+
+        /*---命名規則的にこっちの方が正しい？？【2024/06/06】
+        DB::table('users_table')->insert([
+            ['user_name' => 'Atlas一郎',
+            'user_mail' => 'Atlas01@mail.com',
+            'user_password' => bcrypt(1000)],
+            ['user_name' => 'Atlas二郎',
+            'user_mail' => 'Atlas02@mail.com',
+            'user_password' => bcrypt(2000)],
+            ['user_name' => 'Atlas三郎',
+            'user_mail' => 'Atlas03@mail.com',
+            'user_password' => bcrypt(3000)],
+            ['user_name' => 'Atlas四郎',
+            'user_mail' => 'Atlas04@mail.com',
+            'user_password' => bcrypt(4000)],
+            ['user_name' => 'Atlas五郎',
+            'user_mail' => 'Atlas05@mail.com',
+            'user_password' => bcrypt(5000)]
+        ]);*/
     }
 }
