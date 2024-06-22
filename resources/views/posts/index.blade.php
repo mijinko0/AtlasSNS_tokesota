3f80326f481032111de2bddddf85009c2259a601
mijinko0 <tokesota@gmail.com>
2024-06-22 19:01:36 +0900

    投稿フォーム枠組み準備

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+<!-- ここがTOPページ -->
+<div class="post_form">
+  <!-- 中身要修正 -->
+    <form action="{{ route('post.store') }}" method="POST">
+        <div>
+            名前：
+            <input name="name" value="名前の入力欄"/>
+        </div>
+        <div>
+            タイトル：
+            <input name="title" placeholder="タイトルの入力欄"/>
+        </div>
+        <div>
+            <textarea name="content" placeholder="内容の入力"></textarea>
+        </div>
+        <button>送信</button>
+    </form>
+</div>
 
 @endsection
