ddd4aa780c859c5bf8dcdb96567cc2983dfa9320
mijinko0 <tokesota@gmail.com>
2024-06-22 18:26:44 +0900

    ログアウト機能実装

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,4 +51,10 @@ class LoginController extends Controller
         }
         return view("auth.login");
     }
+
+    protected function logout(Request $request) {
+        Auth::logout();
+        return redirect('login');
+    }
+
 }
