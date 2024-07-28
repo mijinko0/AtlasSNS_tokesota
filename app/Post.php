3bf5a37a405d869eff4c1e75e299ed1a829975b1
mijinko0 <tokesota@gmail.com>
2024-07-28 19:45:05 +0900

    リレーション定義

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $fillable = [
+        'user_id','post'
+    ];
+
+    //「１対多」の「1」側 → メソッド名は単数形でbelongsToを使う
+    //usersテーブルとリレーション
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
