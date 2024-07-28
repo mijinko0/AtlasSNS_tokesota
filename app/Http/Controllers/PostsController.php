a4b0d5e00594d3d6f2c9df4b966baa0dd71a25d5
mijinko0 <tokesota@gmail.com>
2024-07-28 19:46:43 +0900

    ログイン状況に応じたユーザー名、アイコンの表示

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Post;
 
 class PostsController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+        $posts = Post::get();
+        // dd($posts);
+        return view('posts.index',['posts'=>$posts]);
     }
+
+    public function create(Request $request){
+    //投稿内容を保存する処理
+    $user_id = Auth::user()->id;//ログイン情報から参照
+    $post = $request->input('post');//投稿データ参照
+    Post::create([
+           'user_id' => $user_id,
+           'post' => $post,
+        ]);
+
+        return redirect('/top');
+    }
+
 }
